64c20477f429ed1d3a5cd23f670c0589bff4222a
Brett Hevia <brett.hevia@gmail.com>
2016-07-12 15:52:17 -0600

    add buttons and classes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var $urlInput = $('.url-address');
 var $bookmarkButton = $('.bookmark-button')
 
 function makeArticle(string1, string2){
-  $('.bookmark-list').html('<article class="list-item"><p>' + string1 + '</p><p>' + string2 + '</p></article>')}
+  $('.bookmark-list').html('<article class="list-item"><p>' + string1 + '</p><p>' + string2 + '</p><button class="read-button">READ</button><button class="remove-button">REMOVE</button></article>')}
 
   $('.bookmark-button').on('click', function() {
     makeArticle($nameInput.val(), $urlInput.val());
