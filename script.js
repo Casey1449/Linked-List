9c408bacbf63792ff1c756c4e84173a899f2ab73
Brett Hevia <brett.hevia@gmail.com>
2016-07-12 20:55:49 -0600

    add stuff to buttons jscript

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 var $nameInput = $('.bookmark-name');
 var $urlInput = $('.url-address');
 var $bookmarkButton = $('.bookmark-button')
+var $removeButton = $('.remove-button')
 
 function makeArticle(string1, string2){
   $('.bookmark-list').html('<article class="list-item"><p>' + string1 + '</p><p>' + string2 + '</p><button class="read-button">READ</button><button class="remove-button">REMOVE</button></article>')}
@@ -10,3 +11,12 @@ function makeArticle(string1, string2){
   $('.bookmark-button').on('click', function() {
     makeArticle($nameInput.val(), $urlInput.val());
   })
+
+  function removeParentArticle() {
+    $(this).parent().remove();
+  }
+
+  $('.remove-button').on('click', function() {
+    debugger;
+    removeParentArticle();
+  });
