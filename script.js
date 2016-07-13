428ea4e51ecac58eba86592d0e915c12d4c386ed
Brett Hevia <brett.hevia@gmail.com>
2016-07-12 21:42:00 -0600

    add link functionality and error message when an input is blank

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ var $bookmarkButton = $('.bookmark-button')
 var $removeButton = $('.remove-button')
 
 function makeArticle(string1, string2){
-  $('.bookmark-list').html('<article class="list-item"><p>' + string1 + '</p><p>' + string2 + '</p><button class="read-button">READ</button><button class="remove-button">REMOVE</button></article>')}
+  $('.bookmark-list').html('<article class="list-item"><p>' + string1 + '</p><p>' + '<a href="' + string2 + '">' + string2 + '</a>' + '</p><button class="read-button">READ</button><button class="remove-button">REMOVE</button></article>')}
 
   $('.bookmark-button').on('click', function() {
-    makeArticle($nameInput.val(), $urlInput.val());
+    if ($nameInput.val() ==='' || $urlInput.val() === '')
+    {alert("Please enter a name AND a URL")}
+    else {
+      makeArticle($nameInput.val(), $urlInput.val());
+    }
   })
 
   function removeParentArticle() {
