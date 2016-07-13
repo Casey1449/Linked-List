37375804e53f402ae9ce437043a65b7167739a96
Casey Metz <caseymetz@gmail.com>
2016-07-12 18:08:14 -0600

    Get read and remove buttons working, change .html func to .append

--- script.js
+++ script.js
@@ -5,8 +5,31 @@ var $urlInput = $('.url-address');
 var $bookmarkButton = $('.bookmark-button')
 
 function makeArticle(string1, string2){
-  $('.bookmark-list').html('<article class="list-item"><p>' + string1 + '</p><p>' + string2 + '</p><button class="read-button">READ</button><button class="remove-button">REMOVE</button></article>')}
+  $('.bookmark-list').append('<article class="list-item"><p>' + string1 + '</p><p>' + string2 + '</p><button class="read-button">READ</button><button class="remove-button">REMOVE</button></article>')}
 
   $('.bookmark-button').on('click', function() {
     makeArticle($nameInput.val(), $urlInput.val());
   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.bookmark-list').on('click', 'button.read-button', function(){
+    $(this).parent().toggleClass('read');
+});
+
+$('.bookmark-list').on('click', 'button.remove-button', function(){
+    $(this).parent().remove();
+});
